25dcbf134fcb8b97ad956adffea05ed277615924
Hairull03 <mukhlisk514@gmail.com>
2024-09-02 12:36:17 +0700

    spk menggunakan laravel 10

---
+++ database/migrations/2024_06_28_042227_create_nilai_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nilai', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_nama');
+            $table->integer('skor');
+            $table->timestamps();
+
+            $table->foreign('id_nama')->references('id')->on('siswa')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nilai');
+    }
+};
---
+++ resources/views/siswa/siswa.blade.php
@@ -0,0 +1,23 @@
+<div class="card">
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Nama Dokter</th>
+                    <th scope="col">Alamat Dokter</th>
+                    <th scope="col">No. Telephon</th>
+                    <th scope="col">Spesialist</th>
+                    <th scope="col">Deskripsi</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php
+                    $no = 0;
+                @endphp
+            </tbody>
+        </table>
+
+    </div>
+</div>
